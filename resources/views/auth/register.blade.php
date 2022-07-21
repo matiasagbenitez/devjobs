0f90c9dbfadd37350fd01f0e4bc1c2317ee79f23
Matías <matiasagbenitez@gmail.com>
2022-07-20 21:50:12 -0300

    Authentication with Laravel Breeze added

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,79 @@
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
+
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('register') }}" novalidate>
+            @csrf
+
+            <!-- Name -->
+            <div>
+                <x-label for="name" :value="__('Name')" />
+
+                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+            </div>
+
+            <!-- Email Address -->
+            <div class="mt-4">
+                <x-label for="email" :value="__('Email')" />
+
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+            </div>
+
+            <!-- Rol -->
+            <div class="mt-4">
+                <x-label for="role" :value="__('Account type')" />
+
+                <select name="role" id="role" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 w-full">
+                    <option value="">Choose one...</option>
+                    <option value="1">Developer - Find a job</option>
+                    <option value="1">Recruiter - Find an employee</option>
+                </select>
+
+            </div>
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-label for="password" :value="__('Password')" />
+
+                <x-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="new-password" />
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                <x-input id="password_confirmation" class="block mt-1 w-full"
+                                type="password"
+                                name="password_confirmation" required />
+            </div>
+
+            <x-button class="w-full justify-center mt-3">
+                {{ __('Create account') }}
+            </x-button>
+
+            <div class="flex justify-between my-3">
+                @if (Route::has('password.request'))
+
+                    <x-link :href="route('login')">
+                        Already registered? Log in
+                    </x-link>
+
+                    <x-link :href="route('password.request')">
+                        Forgot your password?
+                    </x-link>
+                @endif
+            </div>
+
+        </form>
+    </x-auth-card>
+</x-guest-layout>
